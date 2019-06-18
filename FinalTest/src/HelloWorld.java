6baf4e79686dfbbbec0368a2fdf2ae3d2fe6626f
Pujitha-Rajulapati <pujitha.rajulapati@HYDHTC110928L.ad.infosys.com>
2019-06-18 19:33:21 +0530

    hashmap

--- FinalTest/src/HelloWorld.java
+++ FinalTest/src/HelloWorld.java
@@ -1,9 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("pujitha is idiot");
+		
+		String s1="Pujitha";
+		System.out.println("Hashcode"+s1.hashCode());
+		Map<String,Integer> map1= new HashMap<>();
+		map1.put("JESU", 1);
+		map1.put("KASHIF", 1);
+		map1.put("SIBANI", 1);
+		map1.put("GOURI", 1);
 		System.out.println("Kashif is a good boy");
 	}
 
